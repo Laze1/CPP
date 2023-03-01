81d7d9923fa9595e0a3a4c7eda452f9bb68639cb
吴溪 <546030736@qq.com>
2023-03-01 10:45:04 +0800

    基本使用

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int a;
-    std::cin >> a;
-    std::cout << a+1;
+    unsigned x = 555;
+    unsigned short y = 40000;
+    int du;
+    cout<< "Please enter a Celsius value:";
+    cin>> du;
+    cout<< du <<" degrees Celsius is "<< 1.8*du+32 <<" degrees Fahrenheit";
+    cout << endl << x << " + "<< y;
+    return 0;
 }
